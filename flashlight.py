4721104a93ce5d9958684cb9185a1d2a84dc825d
RahafKHazaa <33985639+RahafKHazaa@users.noreply.github.com>
2019-05-04 00:17:05 +0200

    Update flashlight.py

--- flashlight.py
+++ flashlight.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
-
+cap = cv2.VideoCapture(0)
 pts = []
 while (1):
 
     # Take each frame
+    ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     lower_flashlight = np.array([0, 0, 255])
@@ -15,6 +16,8 @@ while (1):
     cv2.circle(frame, maxLoc, 50, (0, 0, 255), 2, cv2.LINE_AA)
     cv2.imshow('flash', frame)
 
-    
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        break
 
+cap.release()
 cv2.destroyAllWindows()
